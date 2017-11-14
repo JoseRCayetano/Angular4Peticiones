af69f058c6d369dbb363e04f3a74019b8ca60ce0
jrcayetano <jrcayetano@auge.digital>
2017-11-14 16:14:15 +0100

    Poke home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,15 +7,36 @@ import { PokemonRequestService } from '../core/pokemon-request/pokemon-request.s
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  optionsList = [];
+  limit = 150;
+  pokemon;
   constructor(private _pokemonRequestSrv: PokemonRequestService) { }
-
+  
   ngOnInit() {
+    this._generateLis();
     this._pokemonRequestSrv.getPokemon('1').subscribe(pokemon => {
+      this.pokemon = pokemon;
       console.log(pokemon);
     }, error => {
       console.log(error);
     });
   }
 
+  private _generateLis() {
+    for (let i = 0; i < this.limit; i++) {
+      this.optionsList.push({value: i + 1, label: i + 1});
+    }
+  }
+
+  changePokemon(event) {
+    this._pokemonRequestSrv.getPokemon(event).subscribe(pokemon => {
+      this.pokemon = pokemon;
+      console.log(pokemon);
+    }, error => {
+      console.log(error);
+    });
+    console.log(event);
+  }
+
 }
+
